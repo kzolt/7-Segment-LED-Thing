335ea02eb309b02c4470bc9826cb5c4dcf8d58be
Kevin Zoltany <hello@kzoltany.me>
2018-09-19 11:27:06 -0700

    Add project files.

---
+++ LEDSegment/LEDSegment.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LEDSegment.ino
+ Created:	19/09/18 11:26:58
+ Author:	hello
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
